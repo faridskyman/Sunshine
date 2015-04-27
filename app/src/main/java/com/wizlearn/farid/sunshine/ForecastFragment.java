fc124101be54199cc85f006e5c23cef794535fac
faridskyman <farid.skyman@gmail.com>
2015-04-28 02:50:01 +0800

    added menu, webservice call trigger.

--- app/src/main/java/com/wizlearn/farid/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wizlearn/farid/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,17 +28,41 @@ import java.util.List;
  */
 public class ForecastFragment extends Fragment {
 
-    ArrayAdapter<String> mForecastAdapter;
+    public ForecastFragment(){
+        }
 
-    public ForecastFragment(){}
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //handle menu events
+        setHasOptionsMenu(true);
+    }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        Log.i("inflater","Done");
-
-        String[] forecastArray ={
+        String[] data ={
                 "Today - Sunny - 25-35",
                 "Tomorrow - Rainy - 25-35",
                 "Mon - Rainy - 25-35",
@@ -59,17 +86,19 @@ public class ForecastFragment extends Fragment {
                 "Fri - Rainy - 25-35"
         };
 
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
-        mForecastAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_forecast,
-                R.id.list_item_forecast_textview,
-                weekForecast);
+        ArrayAdapter<String> forecastAdapter =
+                new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast);
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView listview = (ListView) rootView.findViewById(R.id.listview_forecast);
-        listview.setAdapter(mForecastAdapter);
+        listview.setAdapter(forecastAdapter);
 
         Log.i("setAdapter", "Done");
 
@@ -125,6 +154,7 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast Data JSOn String:" + forecastJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
