dfdcb84f60ec63bfb994cb2be16387b6f883e00a
faridskyman <farid.skyman@gmail.com>
2015-05-05 22:46:48 +0800

    added intent to other app, map, phone, etc

--- app/src/main/java/com/wizlearn/farid/sunshine/MainActivity.java
+++ app/src/main/java/com/wizlearn/farid/sunshine/MainActivity.java
@@ -1,14 +1,20 @@
 package com.wizlearn.farid.sunshine;
 
+import android.app.SearchManager;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +42,160 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
+        switch (id) {
+
+            case R.id.action_email : {
+                String[] data = {"farid.skyman@gmail.com",};
+                openComposeEmail(data, "This App is cool", null);
+                //break;
+                return true;
+            }
+            case R.id.action_settings: {
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+                //break;
+            }
+
+            case R.id.action_map: {
+                openPreferredLocationInMap();
+                //break;
+                return true;
+            }
+
+            case  R.id.action_phone: {
+                openDialPhoneNumber("97532020");
+                return true;
+
+            }
+
+            case R.id.action_searchWeb :{
+                openSearchWeb("Wizlearn");
+                return true;
+            }
+
+            case R.id.action_sms :{
+                openSMSMessage("Wizlearn App is now available for download",null);
+                return true;
+            }
+
+            case R.id.action_webpage :{
+                openWebPage("http://www.wizlearn.com");
+                return true;
+            }
+
+            default: break;
+
+        }
+
+
         //noinspection SimplifiableIfStatement
+        /*
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        }*/
+
+        /*
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
         }
 
+        */
+
+        /*
+        if (id == R.id.action_email){
+            String[] data ={"farid.skyman@gmail.com",};
+            openComposeEmail(data,"This App is cool", null);
+            return true;
+        }*/
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+
+        //using intent to open the location in maps app
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldnt call " + location + ", no recieving app installed");
+        }
+    }
+
+
+    // sample intent to add calendar
+/*    public void addEvent(String title, String location, Calendar begin, Calendar end) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }*/
+
+    // intent to send email
+    public void openComposeEmail(String[] addresses, String subject, Uri attachment) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:")); //only email app handles this.
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openDialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void openSearchWeb(String query) {
+        Intent intent = new Intent(Intent.ACTION_SEARCH);
+        intent.putExtra(SearchManager.QUERY, query);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openSMSMessage(String message, Uri attachment) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("smsto:"));  // This ensures only SMS apps respond
+        intent.putExtra("sms_body", message);
+        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 
