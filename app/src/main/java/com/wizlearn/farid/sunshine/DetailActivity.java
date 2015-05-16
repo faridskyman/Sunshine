16ff7515206ae0056a61bdee029a01d76267aec3
farid <farid.skyman@gmail.com>
2015-05-16 19:17:14 +0800

    added share but it crash when share but it works, do note.

--- app/src/main/java/com/wizlearn/farid/sunshine/DetailActivity.java
+++ app/src/main/java/com/wizlearn/farid/sunshine/DetailActivity.java
@@ -1,16 +1,21 @@
 package com.wizlearn.farid.sunshine;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.ShareActionProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+//import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 
@@ -56,20 +61,64 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #SunShineApp";
+        private String mForecastString;
+
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            Intent intent = getActivity().getIntent();
+
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Intent intent = getActivity().getIntent();
+
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(forecastStr);
+                        .setText(mForecastString);
             }
             return rootView;
         }
+
+        // for share menu item i recond...
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+            // add the new menu item to te action bar.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            //get the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            //get share provider
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            //attach intent to action provider.
+            if(mShareActionProvider !=null){
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }
+            else{
+                Log.d(LOG_TAG, "Share action provider is null");
+            }
+        }
+
+        private Intent createShareForecastIntent(){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastString + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
+
+
+
     }
 }
